c9ae610dc49609c1dd5282367b147ffbdd3221fb
arpit561 <158806436+arpit561@users.noreply.github.com>
2025-09-08 22:33:07 +0530

    Update environment.js

--- src/configurations/environment.js
+++ src/configurations/environment.js
@@ -1,5 +1,5 @@
 export const ENVIRONMENT_CONFIGS = {
-    BASE_URL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:3020',
+    BASE_URL: import.meta.env.VITE_API_BASE_URL || 'https://ashish2-1.onrender.com',
     GUEST_CREDENTIALS: {
         email: 'guest@gmail.com',
         password: 'Guest6545@'
